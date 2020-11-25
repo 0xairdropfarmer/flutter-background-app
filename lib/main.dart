9eb795214b76b15dca54d9a7dd3801f490ce0f43
Abhishek Tamrakar <razorabhu1995@gmail.com>
2020-11-25 19:54:52 +0545

    wallpaper solution

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:backgroundSolution/screens/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallpaper',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
